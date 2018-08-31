bf633e2a290a4993bf38897c038059bf4a3dafbf
Nick Charchut <ncha@mit.edu>
2018-08-31 15:56:54 -0400

    deleted unnecessary shit

--- totals.py
+++ totals.py
@@ -12,12 +12,6 @@ class CreditTotals(object):
     def _init_totals(self, contacts):
         return {contact.get_name(): PersonTotals(contact, self.tasks) for contact in contacts}
 
-    def load_names_from_gsheets(self):
-        pass
-
-    def load_tasks_from_gsheets(self):
-        pass
-
     def update(self, data):
         # 1. Identify what color it is ( *-1 if red, *1 if green, *0 if neither)
         # 2. Identify name
